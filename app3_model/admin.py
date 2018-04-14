0b876b09f4bbe62765d14d293b53334f2e56f52e
liyueliyue <1048355336@qq.com>
2018-04-14 22:55:05 +0800

    django 后台学习

--- app3_model/admin.py
+++ app3_model/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Person,Course
 
 # Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name','sex','age','address','email')
+class courseAdmin(admin.ModelAdmin):
+    list_display = ('courseName','teacher','score','name')
+admin.site.register(Person,PersonAdmin)
+admin.site.register(Course,courseAdmin)
--- app3_model/models.py
+++ app3_model/models.py
@@ -3,6 +3,16 @@ from django.db import models
 # Create your frommodels here.
 class Person(models.Model):
     name = models.CharField(max_length=10)
+    sex = models.CharField(max_length=10,default='男')
     age = models.IntegerField()
+    address = models.CharField(max_length=225,null=True)
+    email = models.EmailField(null=True)
     def __str__(self):
-        return (self.name,self.age)
+        return self.name
+class Course(models.Model):
+    courseName = models.CharField(max_length=64)
+    teacher = models.CharField(max_length=25)
+    score = models.IntegerField()
+    name = models.CharField(max_length=25)
+    def __str__(self):
+        return self.courseName
